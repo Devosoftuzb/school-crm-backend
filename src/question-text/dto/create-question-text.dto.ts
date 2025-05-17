cce1a8578385891a438704812788d9e5a07c73c7
jurayevd3v <jurayevd3v@gmail.com>
2025-05-17 11:49:03 +0500

    change

--- src/question-text/dto/create-question-text.dto.ts
+++ src/question-text/dto/create-question-text.dto.ts
@@ -2,6 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateQuestionTextDto {
+  @ApiProperty({ example: 'Title', description: 'Question text title' })
+  title: string;
+
   @ApiProperty({ example: 'text', description: 'Question text' })
   text: string;
 }
--- src/question-text/model/question-text.model.ts
+++ src/question-text/model/question-text.model.ts
@@ -1,7 +1,7 @@
 import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript';
 import { Question } from 'src/questions/model/question.model';
 
-@Table({ tableName: 'question-text' })
+@Table({ tableName: 'question_text' })
 export class QuestionText extends Model<QuestionText> {
   @Column({
     type: DataType.INTEGER,
@@ -11,14 +11,20 @@ export class QuestionText extends Model<QuestionText> {
   id: number;
 
   @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  title: string;
+
+  @Column({
     type: DataType.TEXT,
     allowNull: false,
   })
   text: string;
 
-    @HasMany(() => Question, {
-      onDelete: 'CASCADE',
-      hooks: true,
-    })
-    question: Question[];
+  @HasMany(() => Question, {
+    onDelete: 'CASCADE',
+    hooks: true,
+  })
+  question: Question[];
 }
