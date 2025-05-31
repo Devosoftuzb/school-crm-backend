baa118bd3786eb2531c7c1eae2f67bbc790faaeb
jurayevd3v <jurayevd3v@gmail.com>
2025-05-31 12:31:20 +0500

    change

--- src/employee/dto/update-employee.dto.ts
+++ src/employee/dto/update-employee.dto.ts
@@ -1,4 +1,40 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateEmployeeDto } from './create-employee.dto';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
-export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {}
+export class UpdateEmployeeDto {
+  @ApiProperty({ example: 1, description: 'School ID' })
+  @IsNotEmpty()
+  @IsInt()
+  school_id: number;
+
+  @ApiProperty({ example: 'John Doe', description: 'Employee full name' })
+  @IsString()
+  @IsNotEmpty()
+  full_name: string;
+
+  @ApiProperty({
+    example: '=998901234567',
+    description: 'Employee phone number',
+  })
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  phone_number: string;
+
+  @ApiProperty({ example: 'john', description: 'Employee login' })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  login: string;
+
+  @ApiProperty({ example: 'administrator, teacher', description: 'Role name' })
+  @IsString()
+  @IsNotEmpty()
+  role: string;
+}
