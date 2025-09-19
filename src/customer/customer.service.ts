83026133a2c06663a002e4cdf84a3713c8fa0e30
jurayevd3v <jurayevd3v@gmail.com>
2025-09-19 21:59:38 +0500

    change

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -112,7 +112,7 @@ export class CustomerService {
 
   async createWeb(createWebCustomerDto: CreateWebCustomerDto) {
     const customer = await this.repo.create({
-      school_id: 1,
+      school_id: 6,
       full_name: createWebCustomerDto.full_name,
       phone_number: createWebCustomerDto.phone_number,
       subject_id: createWebCustomerDto.subject_id,
