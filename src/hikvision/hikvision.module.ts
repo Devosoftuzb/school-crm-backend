fcbe4367432bef4cf7bfa09b016ccbc396388c33
jurayevd3v <jurayevd3v@gmail.com>
2026-03-15 19:22:31 +0500

    update

--- src/hikvision/hikvision.module.ts
+++ src/hikvision/hikvision.module.ts
@@ -5,9 +5,10 @@ import { HikvisionService } from './hikvision.service';
 import { HikvisionController } from './hikvision.controller';
 import { Student } from 'src/student/models/student.model';
 import { StudentAttendance } from 'src/student_attendance/models/student_attendance.model';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Student, StudentAttendance])],
+  imports: [SequelizeModule.forFeature([Student, StudentAttendance]), JwtModule],
   providers: [HikvisionService],
   controllers: [HikvisionController],
   exports: [HikvisionService],
