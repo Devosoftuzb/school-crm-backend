202c7ebcc504d01587075573304f91fed34faa0a
jurayevd3v <jurayevd3v@gmail.com>
2026-03-15 14:59:38 +0500

    update

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -6,9 +6,10 @@ import { Attendance } from './models/attendance.model';
 import { JwtModule } from '@nestjs/jwt';
 import { Student } from 'src/student/models/student.model';
 import { SmsModule } from 'src/sms/sms.module';
+import { Group } from 'src/group/models/group.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Attendance, Student]), JwtModule],
+  imports: [SequelizeModule.forFeature([Attendance, Student, Group]), JwtModule],
   controllers: [AttendanceController],
   providers: [AttendanceService],
 })
